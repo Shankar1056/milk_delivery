e4b238c7a37abf703cdd696478b6f48d46bd9098
Shankar1056 <dayashankargupta86@gmail.com>
2018-10-28 00:15:36 +0530

    first commit

---
+++ app/src/main/java/apextechies/sadf/AfterSplash.kt
@@ -0,0 +1,12 @@
+package apextechies.sadf
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AfterSplash: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aftersplash)
+    }
+}
---
+++ app/src/main/java/apextechies/sadf/SplashActivity.java
@@ -0,0 +1,87 @@
+package apextechies.sadf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * Created by Shankar on 3/30/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    public static final int RC_SIGN_IN = 111;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+       // FirebaseApp.initializeApp(this);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        Thread timer= new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    //Display for 3 seconds
+                    sleep(3000);
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    startActivity(new Intent(SplashActivity.this, AfterSplash.class));
+                    finish();
+                    //calApi();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    private void calApi() {
+       /* Download_web web = new Download_web(SplashActivity.this, new OnTaskCompleted() {
+            @Override
+            public void onTaskCompleted(String response) {
+
+                try {
+                    JSONObject jsonObject  = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    for (int i=0;i<jsonArray.length(); i++){
+                        JSONObject jsonObject1  = jsonArray.getJSONObject(0);
+                        if (jsonObject1.getString("status").equals("0")){
+                            Utilz.displayMessageAlert("oohh, App not woring? if you are admin, please call 8002877277", SplashActivity.this);
+                        }else {
+                            if (ClsGeneral.getPreferences(SplashActivity.this,ConstantValue.USERID).equals("")){
+                                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                                finish();
+                            }else {
+                                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        }
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        web.setReqType(true);
+        web.execute("http://apextechies.com/englishbolega/index.php/start_stop_app");
+ */   }
+
+
+}
